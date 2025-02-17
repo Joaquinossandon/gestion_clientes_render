857ca1c6d22c6aef50be273b5769c66f5d7dc0f7
Joaquinossandon <joaquin.ossandon@inacapmail.cl>
2025-02-16 22:02:56 -0300

    refactor database connection to use a single DATABASE_URL and simplify configuration

--- config.js
+++ config.js
@@ -3,12 +3,8 @@ dotenv.config()
 
 const envs = {
   PORT: process.env.PORT || 3000,
-  DB_PORT: process.env.DB_PORT || 5432,
-  DB_HOST: process.env.DB_HOST || 'localhost',
-  DB_USER: process.env.DB_USER || 'postgres',
-  DB_PASSWORD: process.env.DB_PASSWORD || 'postgres',
-  DB_NAME: process.env.DB_NAME || 'postgres'
+  DB_URL: process.env.DATABASE_URL,
 }
 
-module.exports = { PORT, DB_PORT, DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } =
+module.exports = { PORT, DB_URL } =
   envs
--- connection.js
+++ connection.js
@@ -2,11 +2,7 @@ const { Pool } = require('pg')
 const envs = require('./config')
 
 const pool = new Pool({
-  host: envs.DB_HOST,
-  user: envs.DB_USER,
-  password: envs.DB_PASSWORD,
-  database: envs.DB_NAME,
-  port: envs.DB_PORT,
+  connectionString: envs.DB_URL,
   ssl: {
     rejectUnauthorized: false
   }
